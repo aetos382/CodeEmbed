8be6178970e6bc35b36af1a1cc1f3bc5e2f5865c
aetos <aetos@aerie.jp>
2015-01-21 02:36:35 +0900

    change

---
+++ beforeBuild.ps1
@@ -0,0 +1,12 @@
+﻿Push-Location $PSScriptRoot
+
+try {
+	$path = Convert-Path .\CodeEmbed.Web.Api\Web.config
+	$xml = [xml] (Get-Content $path)
+	$node = $xml.SelectSingleNode('/configuration/applicationSettings/CodeEmbed.Web.Api.Properties.Settings/setting[@name = "AccessToken"]')
+	$node.value = $env:github_oauth_token
+	$xml.Save($path)
+}
+finally {
+	Pop-Location
+}
