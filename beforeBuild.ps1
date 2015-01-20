bacb0fac4ecbf40dbdb8a31019dce0a9ca6d3f9c
aetos <aetos@aerie.jp>
2015-01-21 07:24:56 +0900

    remove core tests

--- beforeBuild.ps1
+++ beforeBuild.ps1
@@ -1,11 +1,23 @@
 ﻿Push-Location $PSScriptRoot
 
+$env:github_oauth_token
+
 try {
 	$path = Convert-Path .\CodeEmbed.Web.Api\Web.config
 	$xml = [xml] (Get-Content $path)
 	$node = $xml.SelectSingleNode('/configuration/applicationSettings/CodeEmbed.Web.Api.Properties.Settings/setting[@name = "AccessToken"]')
 	$node.value = $env:github_oauth_token
 	$xml.Save($path)
+}
+finally {
+	Pop-Location
+}
+
+try {
+	$path = Convert-Path .\CodeEmbed.Web.Api\Web.config
+	$xml = [xml] (Get-Content $path)
+	$node = $xml.SelectSingleNode('/configuration/applicationSettings/CodeEmbed.Web.Api.Properties.Settings/setting[@name = "AccessToken"]')
+	$node.value
 }
 finally {
 	Pop-Location
