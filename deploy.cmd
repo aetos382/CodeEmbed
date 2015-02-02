10a6c27cc0dfffc3c061c19346b0c20a85ae3748
aetos <aetos@aerie.jp>
2015-02-02 17:52:04 +0900

    deploy

--- deploy.cmd
+++ deploy.cmd
@@ -1,4 +1,10 @@
 @echo off
 
-"%ChocolateyInstall%\bin\WAWSDeploy.exe" Publish\Debug\CodeEmbed.Web.Site codeembed-dev.PublishSettings /p %deploy_password% /d /t site
-"%ChocolateyInstall%\bin\WAWSDeploy.exe" Publish\Debug\CodeEmbed.Web.Api codeembed-dev.PublishSettings /p %deploy_password% /d /t api\v1
+if "%Configuration%" == "Release" goto DEPLOY_RELEASE
+
+"%ChocolateyInstall%\bin\WAWSDeploy.exe" Publish\CodeEmbed.Web.Site codeembed-dev.PublishSettings /p %deploy_password% /d /t site
+"%ChocolateyInstall%\bin\WAWSDeploy.exe" Publish\CodeEmbed.Web.Api codeembed-dev.PublishSettings /p %deploy_password% /d /t api\v1
+
+:DEPLOY_RELEASE
+
+exit /b
