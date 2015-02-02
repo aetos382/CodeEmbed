7f598d65fb2d6aef35c6652d19f3dc38fe5574f8
aetos <aetos@aerie.jp>
2015-02-02 18:52:53 +0900

    verbose loggin

--- deploy.cmd
+++ deploy.cmd
@@ -2,8 +2,8 @@
 
 if "%Configuration%" == "Release" goto DEPLOY_RELEASE
 
-"%ChocolateyInstall%\bin\WAWSDeploy.exe" Publish\CodeEmbed.Web.Site codeembed-dev.PublishSettings /p %deploy_password% /d /t site
-"%ChocolateyInstall%\bin\WAWSDeploy.exe" Publish\CodeEmbed.Web.Api codeembed-dev.PublishSettings /p %deploy_password% /d /t api\v1
+"%ChocolateyInstall%\bin\WAWSDeploy.exe" Publish\CodeEmbed.Web.Site codeembed-dev.PublishSettings /p %deploy_password% /d /v /t site
+"%ChocolateyInstall%\bin\WAWSDeploy.exe" Publish\CodeEmbed.Web.Api codeembed-dev.PublishSettings /p %deploy_password% /d /v /t api\v1
 
 :DEPLOY_RELEASE
 
