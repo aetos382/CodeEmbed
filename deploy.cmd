8614179ad75204edf2927ec9b1abe463a912df1c
aetos <aetos@aerie.jp>
2015-02-02 19:40:32 +0900

    disable verbose logging

--- deploy.cmd
+++ deploy.cmd
@@ -3,7 +3,7 @@
 if "%Configuration%" == "Release" goto DEPLOY_RELEASE
 
 "%ChocolateyInstall%\bin\WAWSDeploy.exe" Publish\CodeEmbed.Web.Site codeembed-dev.PublishSettings /p %deploy_password% /d
-"%ChocolateyInstall%\bin\WAWSDeploy.exe" Publish\CodeEmbed.Web.Api codeembed-dev.PublishSettings /p %deploy_password% /d /v /t api/v1
+"%ChocolateyInstall%\bin\WAWSDeploy.exe" Publish\CodeEmbed.Web.Api codeembed-dev.PublishSettings /p %deploy_password% /d /t api/v1
 
 :DEPLOY_RELEASE
 
