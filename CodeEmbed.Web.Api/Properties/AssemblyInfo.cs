745804b48c8d519b00391eb1272a0c8b4ea7cbdd
aetos <aetos@aerie.jp>
2015-02-13 15:30:59 +0900

    comment only

--- CodeEmbed.Web.Api/Properties/AssemblyInfo.cs
+++ CodeEmbed.Web.Api/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 
-// Octokig が CLS 非準拠
+// Octokit が CLS 非準拠
 [assembly: CLSCompliant(false)]
