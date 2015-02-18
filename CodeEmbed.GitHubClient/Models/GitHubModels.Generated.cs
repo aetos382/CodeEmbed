214fe8bd9a66cc8b02c9117347e79ae59f8131f8
aetos <aetos@aerie.jp>
2015-02-19 02:19:29 +0900

    model generator

--- CodeEmbed.GitHubClient/Models/GitHubModels.Generated.cs
+++ CodeEmbed.GitHubClient/Models/GitHubModels.Generated.cs
@@ -7,20 +7,22 @@
 
 
 
+
 namespace CodeEmbed.GitHubClient.Models
 {
 	using System;
 	using System.CodeDom.Compiler;
 	using System.Diagnostics.Contracts;
 	
-
 	[GeneratedCode("ModelInterface.tt", "1.0")]
 	public interface IRepositoryGenerated
 	{
 		// name
 		string Name { [Pure] get; }
+
 		// url
 		string Uri { [Pure] get; }
+
 	}
 }
 
@@ -37,17 +39,36 @@ namespace CodeEmbed.GitHubClient.Models
 	using System.Diagnostics;
 	using System.Diagnostics.Contracts;
 	
+	using CodeEmbed.GitHubClient;
 
 	[GeneratedCode("ModelClass.tt", "1.0")]
 	public class RepositoryGenerated :
 		IRepositoryGenerated
 	{
+		[ContractPublicPropertyName("Client")]
+		private readonly IGitHubClient _client;
+
 		private readonly IRepositoryGenerated _repository;
 
 		public RepositoryGenerated(
+			IGitHubClient client,
 			IRepositoryGenerated repository)
 		{
+			Contract.Requires<ArgumentNullException>(client != null);
+			Contract.Requires<ArgumentNullException>(repository != null);
+
+			this._client = client;
 			this._repository = repository;
+		}
+
+		public IGitHubClient Client
+		{
+			get
+			{
+				Contract.Ensures(Contract.Result<IGitHubClient>() != null);
+
+				return this._client;
+			}
 		}
 
 		public string Name
@@ -73,6 +94,7 @@ namespace CodeEmbed.GitHubClient.Models
         [ContractInvariantMethod]
         private void ObjectInvariant()
         {
+			Contract.Invariant(this._client != null);
             Contract.Invariant(this._repository != null);
         }
 	}
@@ -92,7 +114,6 @@ namespace CodeEmbed.GitHubClient.Models.Serialization
 	
 	using CodeEmbed.GitHubClient.Models;
 
-
 	[GeneratedCode("SerializableClass.tt", "1.0")]
 	[JsonObject]
 	public class SerializableRepositoryGenerated :
@@ -101,6 +122,7 @@ namespace CodeEmbed.GitHubClient.Models.Serialization
 		private readonly string _name;
 		private readonly string _uri;
 
+		[JsonConstructor]
 		public SerializableRepositoryGenerated(
 			string name,
 			string uri)
@@ -140,14 +162,15 @@ namespace CodeEmbed.GitHubClient.Models.Internal
 	using System.CodeDom.Compiler;
 	using System.Diagnostics.Contracts;
 	
-
 	[GeneratedCode("ModelInterface.tt", "1.0")]
 	public interface IRepositoryUserGenerated
 	{
 		// name
 		string Name { [Pure] get; }
+
 		// url
 		string Uri { [Pure] get; }
+
 	}
 }
 
@@ -164,7 +187,6 @@ namespace CodeEmbed.GitHubClient.Models.Internal
 	using System.Diagnostics;
 	using System.Diagnostics.Contracts;
 	
-
 	[GeneratedCode("ModelClass.tt", "1.0")]
 	public class RepositoryUserGenerated :
 		IRepositoryUserGenerated
@@ -174,6 +196,8 @@ namespace CodeEmbed.GitHubClient.Models.Internal
 		public RepositoryUserGenerated(
 			IRepositoryUserGenerated repositoryUser)
 		{
+			Contract.Requires<ArgumentNullException>(repositoryUser != null);
+
 			this._repositoryUser = repositoryUser;
 		}
 
@@ -219,7 +243,6 @@ namespace CodeEmbed.GitHubClient.Models.Serialization
 	
 	using CodeEmbed.GitHubClient.Models.Internal;
 
-
 	[GeneratedCode("SerializableClass.tt", "1.0")]
 	[JsonObject]
 	public class SerializableRepositoryUserGenerated :
@@ -228,6 +251,7 @@ namespace CodeEmbed.GitHubClient.Models.Serialization
 		private readonly string _name;
 		private readonly string _uri;
 
+		[JsonConstructor]
 		public SerializableRepositoryUserGenerated(
 			string name,
 			string uri)
@@ -251,6 +275,120 @@ namespace CodeEmbed.GitHubClient.Models.Serialization
 			get
 			{
 				return this._uri;
+			}
+		}
+
+	}
+}
+
+
+
+
+
+namespace CodeEmbed.GitHubClient.Models
+{
+	using System;
+	using System.CodeDom.Compiler;
+	using System.Diagnostics.Contracts;
+	
+	using CodeEmbed.GitHubClient.Models.Internal;
+
+	[GeneratedCode("ModelInterface.tt", "1.0")]
+	public interface IDetailedUserGenerated
+		: IRepositoryUserGenerated
+	{
+		// hoge
+		string Hoge { [Pure] get; }
+
+	}
+}
+
+
+
+
+
+
+namespace CodeEmbed.GitHubClient.Models
+{
+	using System;
+	using System.CodeDom.Compiler;
+	using System.ComponentModel;
+	using System.Diagnostics;
+	using System.Diagnostics.Contracts;
+	
+	using CodeEmbed.GitHubClient.Models.Internal;
+
+	[GeneratedCode("ModelClass.tt", "1.0")]
+	public class DetailedUserGenerated :
+		RepositoryUserGenerated,
+		IDetailedUserGenerated
+	{
+		private readonly IDetailedUserGenerated _detailedUser;
+
+		public DetailedUserGenerated(
+			IDetailedUserGenerated detailedUser)
+			: base(detailedUser)
+		{
+			Contract.Requires<ArgumentNullException>(detailedUser != null);
+
+			this._detailedUser = detailedUser;
+		}
+
+		public string Hoge
+		{
+			get
+			{
+				return this._detailedUser.Hoge;
+			}
+		}
+
+        [Conditional("CONTRACTS_FULL")]
+        [DebuggerStepThrough]
+        [DebuggerHidden]
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        [ContractInvariantMethod]
+        private void ObjectInvariant()
+        {
+            Contract.Invariant(this._detailedUser != null);
+        }
+	}
+}
+
+
+
+
+
+
+namespace CodeEmbed.GitHubClient.Models.Serialization
+{
+	using System;
+	using System.CodeDom.Compiler;
+
+	using Newtonsoft.Json;
+	
+	using CodeEmbed.GitHubClient.Models.Internal;
+
+	[GeneratedCode("SerializableClass.tt", "1.0")]
+	[JsonObject]
+	public class SerializableDetailedUserGenerated :
+		SerializableRepositoryUserGenerated,
+		IDetailedUserGenerated
+	{
+		private readonly string _hoge;
+
+		[JsonConstructor]
+		public SerializableDetailedUserGenerated(
+			string hoge)
+		{
+			this._hoge = hoge;
+		}
+
+		[JsonProperty("hoge")]
+		public string Hoge
+		{
+			get
+			{
+				return this._hoge;
 			}
 		}
 
