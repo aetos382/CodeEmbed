9c4e7013719df926a7f93495be2b0090b7b8817e
aetos <aetos@aerie.jp>
2015-03-20 15:08:02 +0900

    before build script

--- beforeBuild.ps1
+++ beforeBuild.ps1
@@ -6,14 +6,21 @@
 
 	$fullpath = Convert-Path $Path
 	$xml = [xml] (Get-Content $fullpath)
-	$node = $xml.SelectSingleNode('/configuration/appSettings/add[@key = "OAuthToken"]')
-	$node.value = $env:github_oauth_token
+	
+	$element = $xml.CreateElement('add')
+	$element.SetAttribute('key', 'OAuthToken')
+	$element.SetAttribute('value', $env:github_oauth_token)
+	
+	$settings = $xml.SelectSingleNode('/configuration/appSettings')
+	$settings.AppendChild($element)
+	
 	$xml.Save($fullpath)
 }
 
 Push-Location $PSScriptRoot
 
 try {
+	Set-OAuthToken .\CodeEmbed.GitHubClient.Tests\App.config
 	Set-OAuthToken .\CodeEmbed.Web.Api\Web.config
 	Set-OAuthToken .\CodeEmbed.Web.Api.Tests\App.config
 }
