f8cbded9fd088212411ab983a019592536d416c2
aetos <aetos@aerie.jp>
2015-03-21 01:13:12 +0900

    fix deploy script

--- deploy-dev.cmd
+++ deploy-dev.cmd
@@ -4,8 +4,6 @@ setlocal
 
 set EXIT_CODE=0
 
-if /i "%Configuration%" NEQ "Debug" set EXIT_CODE=1 && goto DEPLOY_END
-
 "%ChocolateyInstall%\bin\WAWSDeploy.exe" Publish\CodeEmbed.Web.Site codeembed-dev.PublishSettings /p %deploy_password% /d
 if ERRORLEVEL 1 set EXIT_CODE=%ERRORLEVEL% && goto DEPLOY_END
 
--- deploy-release.cmd
+++ deploy-release.cmd
@@ -4,8 +4,7 @@ setlocal
 
 set EXIT_CODE=0
 
-if /i "%Configuration%" NEQ "Release" set EXIT_CODE=1 && goto DEPLOY_END
-if /i "%APPVEYOR_REPO_TAG%" NEQ "true" set EXIT_CODE=1 && goto DEPLOY_END
+if /i "%APPVEYOR_REPO_TAG%" NEQ "true" GOTO DEPLOY_END
 
 "%ChocolateyInstall%\bin\WAWSDeploy.exe" Publish\CodeEmbed.Web.Site codeembed.PublishSettings /p %deploy_password% /d
 if ERRORLEVEL 1 set EXIT_CODE=%ERRORLEVEL% && goto DEPLOY_END
