e4367a9def7a325c35db681f4be71a274b2965aa
aetos <aetos@aerie.jp>
2015-03-26 19:01:42 +0900

    test

---
+++ Bundle-Reference.ps1
@@ -0,0 +1,87 @@
+﻿function Bundle-Reference {
+
+  [CmdletBinding(SupportsShouldProcess, ConfirmImpact = 'Medium', DefaultParameterSetName = 'Path')]
+
+  param(
+    [Parameter(ParameterSetName = 'Path', Mandatory, Position = 0, ValueFromPipeline, ValueFromPipelineByPropertyName)]
+    [SupportsWildcards()]
+    [string[]] $Path,
+
+    [Parameter(ParameterSetName = 'LiteralPath', Mandatory, Position = 0, ValueFromPipeline, ValueFromPipelineByPropertyName)]
+    [Alias('PSPath')]
+    [string[]] $LiteralPath,
+    
+    [string] $BeginBlockPattern = '^(\s*)<!-- begin bundle:(.+?) -->\s*$',
+    
+    [string] $EndBlockPattern = '<!-- end bundle -->',
+    
+    [scriptblock] $Script)
+
+  process {
+  
+    if ($PSCmdlet.ParameterSetName -eq 'Path') {
+      $files = Resolve-Path -Path $Path
+    }
+    else {
+      $files = Resolve-Path -Path $LiteralPath
+    }
+
+    @($files) | % {
+
+      $file = $_
+
+      if (!$PSCmdlet.ShouldProcess($file)) {
+        return
+      }
+
+      $bundling = $false
+
+      $lines = Get-Content $file
+
+      $output = @($lines) | % {
+
+        $line = $_
+
+        if ($line -match $BeginBlockPattern) {
+          if ($Script) {
+            $result = & $Script $line $Matches
+          }
+          else {
+            $leadingSpace = $Matches[1]
+            $bundle = $Matches[2]
+
+            "${leadingSpace}${bundle}"
+          }
+
+          $bundling = $true
+        }
+        elseif ($line -match $EndBlockPattern) {
+          if ($bundling) {
+            $bundling = $false
+          }
+          else {
+            $line
+          }
+        }
+        else {
+          if (!$bundling) {
+            $line
+          }
+        }
+
+      }
+
+      Set-Content $file -Value $output -Encoding 'UTF8'
+    }
+
+  }
+}
+
+Push-Location $PSScriptRoot
+
+try {
+	Get-ChildItem 'CodeEmbed.Web.Site' -Filter '*.cshtml' -Recurse | Bundle-Reference
+}
+finally {
+	Pop-Location
+}
--- preBuild.cmd
+++ preBuild.cmd
@@ -9,9 +9,6 @@ set EXIT_CODE=0
 call CodeEmbed.Web.Site\preBuild.cmd
 if errorlevel 1 set EXIT_CODE=%ERRORLEVEL% && goto END_PREBUILD
 
-call transform.cmd
-if errorlevel 1 set EXIT_CODE=%ERRORLEVEL% && goto END_PREBUILD
-
 :END_PREBUILD
 
 popd
